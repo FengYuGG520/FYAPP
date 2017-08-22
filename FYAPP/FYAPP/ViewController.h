7a3b25f37e0c186306609aa91e9c8aefc41f8c9b
华融世全 <2972680549@qq.com>
2017-08-22 15:48:46 +0800

    1. Team: None 2. Language: Objective-C 3. Use Core Data 4. Include Unit Tests 5. Include UI Tests

---
+++ FYAPP/FYAPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FYAPP
+//
+//  Created by 夜枫宇 on 2014/8/22.
+//  Copyright © 2014年 fengyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FYAPP/FYAPP/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FYAPP
+//
+//  Created by 夜枫宇 on 2014/8/22.
+//  Copyright © 2014年 fengyu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
