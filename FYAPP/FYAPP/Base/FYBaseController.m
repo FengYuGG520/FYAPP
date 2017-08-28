d6c3d95e01dddf1864eb7aecff90768f97b0113b
FengYu <2972680549@qq.com>
2017-08-28 20:07:11 +0800

    1. 调整

--- FYAPP/FYAPP/Base/FYBaseController.m
+++ FYAPP/FYAPP/Base/FYBaseController.m
@@ -1,11 +1,3 @@
-//
-//  FYBaseController.m
-//  FYAPP
-//
-//  Created by 夜枫宇 on 2014/8/22.
-//  Copyright © 2014年 fengyu. All rights reserved.
-//
-
 #import "FYBaseController.h"
 
 @interface FYBaseController ()
--- FYAPP/FYAPP/ViewController.m
+++ FYAPP/FYAPP/ViewController.m
@@ -8,6 +8,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
 }
 
 
