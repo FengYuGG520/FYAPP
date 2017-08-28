f8d705e985bd4bc3644757bede543d14d2cc6930
FengYu <2972680549@qq.com>
2017-08-28 09:57:20 +0800

    1. 新建 Base 模块 2. 新建 FYBaseController

---
+++ FYAPP/FYAPP/Base/FYBaseController.m
@@ -0,0 +1,36 @@
+//
+//  FYBaseController.m
+//  FYAPP
+//
+//  Created by 夜枫宇 on 2014/8/22.
+//  Copyright © 2014年 fengyu. All rights reserved.
+//
+
+#import "FYBaseController.h"
+
+@interface FYBaseController ()
+
+@end
+
+@implementation FYBaseController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // MARK: - 搭建界面
+    [self setupUI];
+    
+    // MARK: - 加载数据
+    [self loadData];
+}
+
+// MARK: - 搭建界面
+- (void)setupUI {
+    
+}
+// MARK: - 加载数据
+- (void)loadData {
+    
+}
+
+@end
