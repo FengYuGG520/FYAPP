6d0e9e26584ff82b16c98826b71d61d72e166246
FengYu <2972680549@qq.com>
2017-08-28 21:33:17 +0800

    1. 增加 runtime 分类的功能

--- FYAPP/FYAPP/FYAPP/FY_Runtime/NSObject+Runtime.h
+++ FYAPP/FYAPP/FYAPP/FY_Runtime/NSObject+Runtime.h
@@ -8,13 +8,28 @@
 
 #import <Foundation/Foundation.h>
 
+/// 运行时 runtime 的本质就是用一组指针去记录一个类的细节
+/// 运行时的作用让闭源的 ios 变得开源
+
+// OC 对象的指针组成
+// 类: class ---- isa指针
+// 属性: property (最多)
+// 方法: Method ---- IMP指针 (很多)
+// 成员变量: ivar指针 (很少)
+// 协议: protocol (极少)
+
 @interface NSObject (Runtime)
 
-/**
- 获取该对象的所有属性列表
- 
- @return 该对象的所有属性列表
- */
-- (NSArray *)propsList;
+// 获取对象所有的属性
+- (NSArray *)getPropertylist;
+
+// 获取对象所有属性及属性的值  key(属性)=value(值)
+- (NSArray <NSDictionary *>*)getPropertyAndValueList;
+
+// 获取对象所有的方法
+- (NSArray *)getMethodlist;
+
+// 获取对象所有的成员变量
+- (NSArray *)getIvarlist;
 
 @end
--- FYAPP/FYAPP/FYAPP/FY_Runtime/NSObject+Runtime.m
+++ FYAPP/FYAPP/FYAPP/FY_Runtime/NSObject+Runtime.m
@@ -11,29 +11,139 @@
 
 @implementation NSObject (Runtime)
 
-- (NSArray *)propsList {
+// 获取对象所有的属性
+- (NSArray *)getPropertylist
+{
+    NSMutableArray *arr = [NSMutableArray array];
+    // 0. 属性的数量
     unsigned int count = 0;
-    /**
-     *  1: 类
-     *  2: 成员变量的计数
-     *  返回的是 C 语言的数组
-     */
-    Ivar *ivarList = class_copyIvarList([self class], &count);
-    // 创建成员变量数组
-    NSMutableArray *ivars = [NSMutableArray new];
-    // 遍历数组
-    for (int i = 0; i < count; i ++) {
-        // 1. 根据下标获取成员变量
-        Ivar ivar = ivarList[i];
-        // 2. 获取成员变量的名称
-        const char *cName = ivar_getName(ivar);
-        // 3. 转换成 NSString
-        NSString *name = [NSString stringWithCString:cName encoding:NSUTF8StringEncoding];
-        [ivars addObject:name];
+    // 1. 获取类所有的属性
+    // 第一个参数:哪一个类  第二个参数:用于接收属性的数量的地址 返回值是一个结构体数组
+    objc_property_t *propertyList = class_copyPropertyList([self class], &count);
+    // 2. 遍历属性数组, 获取属性的详细信息
+    for (int i =0; i<count; i++) {
+        objc_property_t property = propertyList[i];
+        
+        // 3. 获取属性的名称
+        const char *name = property_getName(property);
+        // c字符串转成oc字符串
+        NSString *nameStr = [[NSString alloc] initWithUTF8String:name];
+        
+        [arr addObject:nameStr];
     }
-    // 释放对象 free
-    free(ivarList);
-    return ivars.copy;
+    
+    // C对象都需要手动释放(一般带*需要释放)
+    free(propertyList);
+    
+    return [arr copy];
+}
+
+
+// 获取对象所有属性及属性的值 key(属性)=value(值)
+- (NSArray <NSDictionary *>*)getPropertyAndValueList
+{
+    NSMutableArray *arr = [NSMutableArray array];
+    // 0 属性的数量
+    unsigned int count = 0;
+    // 1. 获取类所有的属性
+    // 第一个参数:哪一个类  第二个参数:用于接收属性的数量的地址 返回值是一个结构体数组
+    objc_property_t *propertyList = class_copyPropertyList([self class], &count);
+    // 2. 遍历属性数组,获取属性的详细信息
+    for (int i =0; i<count; i++) {
+        objc_property_t property = propertyList[i];
+        
+        // 3. 获取属性的名称
+        const char *name = property_getName(property);
+        // c字符串转成oc字符串
+        NSString *nameStr = [[NSString alloc] initWithUTF8String:name];
+        // 4. 获取属性的值
+        
+        // 通过kvc取值
+        NSString *value = [self valueForKey:nameStr];
+        
+        NSDictionary *dic = @{nameStr:value};
+        
+        [arr addObject:dic];
+    }
+    
+    // C对象都需要手动释放(一般带*需要释放)
+    free(propertyList);
+    
+    return [arr copy];
+}
+
+- (id)valueForUndefinedKey:(NSString *)key
+{
+    return nil;
+}
+
+// 获取对象所有的方法
+- (NSArray *)getMethodlist
+{
+    NSMutableArray *arr = [NSMutableArray array];
+    // 0 方法的数量
+    unsigned int count = 0;
+    // 1. 获取类所有的属性
+    // 第一个参数:哪一个类  第二个参数:用于接收方法的数量的地址 返回值是一个结构体数组
+    Method *methodList = class_copyMethodList([self class], &count);
+    // 2. 遍历方法数组,获取方法的详细信息
+    for (int i =0; i<count; i++) {
+        Method method = methodList[i];
+        
+        // 3. 获取方法签名
+        SEL sel =  method_getName(method);
+        
+        
+        
+        // 4. 获取方法名字符串
+        const char *methodName = sel_getName(sel);
+        
+        // c字符串转成oc字符串
+        NSString *methodStr = [[NSString alloc] initWithUTF8String:methodName];
+        
+        // 5. 获取方法的数量
+        //        unsigned int argumentCount =  method_getNumberOfArguments(method);
+        //        for (i = 0 ; i<argumentCount; i++) {
+        //            char name;
+        //            method_getArgumentType(method, i, &name, 8);
+        //            NSLog(@"%c",name);
+        //        }
+        
+        [arr addObject:methodStr];
+    }
+    
+    // C对象都需要手动释放(一般带*需要释放)
+    free(methodList);
+    
+    return [arr copy];
+}
+
+// 获取对象所有的成员变量
+- (NSArray *)getIvarlist
+{
+    NSMutableArray *arr = [NSMutableArray array];
+    // 0 成员变量的数量
+    unsigned int count = 0;
+    // 1. 获取类所有的属性
+    // 第一个参数:哪一个类  第二个参数:用于接收属性的数量的地址 返回值是一个结构体数组
+    Ivar *propertyList = class_copyIvarList([self class], &count);
+    
+    // 2. 遍历属性数组,获取属性的详细信息
+    for (int i =0; i<count; i++) {
+        Ivar property = propertyList[i];
+        
+        // 3. 获取属性的名称
+        const char *name = ivar_getName(property);
+        // c字符串转成oc字符串
+        NSString *nameStr = [[NSString alloc] initWithUTF8String:name];
+        
+        [arr addObject:nameStr];
+    }
+    
+    // C对象都需要手动释放(一般带*需要释放)
+    free(propertyList);
+    
+    return [arr copy];
 }
 
 @end
